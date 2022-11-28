e684d94fcd54325ea4f16a0274768061e2f1deb6
yarmail <yarmail@yandex.ru>
2022-11-28 20:47:09 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,4 +1,21 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Метод index принимает объект Model. Объект модель
+ * - это оберта над классом HttpServletRequest.
+ * То есть через объект Model можно получить данные
+ * из запроса или отправить данные в ответ.
+ */
+@Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", "name1");
+        return "index";
+    }
 }
