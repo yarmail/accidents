19d287de4ca86718402fac362fd07b76e4770fd0
yarmail <yarmail@yandex.ru>
2022-11-28 17:48:28 +0300

    1. Техническое задание - проект "Автонарушители" [#260365]

---
+++ src/main/java/ru/job4j/accidents/AccidentsApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.accidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccidentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/job4j/accidents/AccidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.accidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccidentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
