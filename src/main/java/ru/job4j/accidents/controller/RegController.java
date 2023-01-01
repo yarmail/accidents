b16f8d86048cc48a276c650a1fe0998c363f979a
yarmail <yarmail@yandex.ru>
2023-01-01 23:41:49 +0300

    2. Регистрация пользователя [#296069 #376312]

--- src/main/java/ru/job4j/accidents/controller/RegController.java
+++ src/main/java/ru/job4j/accidents/controller/RegController.java
@@ -1,11 +1,14 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
 import ru.job4j.accidents.repository.UserRepository;
@@ -16,6 +19,7 @@ public class RegController {
     private final PasswordEncoder encoder;
     private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
+    private static final Logger LOG = LogManager.getLogger(RegController.class);
 
     @PostMapping("/reg")
     public String regSave(@ModelAttribute User user) {
@@ -23,11 +27,23 @@ public class RegController {
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorityRepository.findByAuthority("ROLE_USER"));
         userRepository.save(user);
-        return "redirect:login";
+        return "redirect:/login";
     }
 
     @GetMapping("/reg")
-    public String regPage() {
+    public String regPage(@RequestParam(value = "error", required = false)
+                          String error, Model model) {
+        String errorMessage = null;
+        if (error != null) {
+            errorMessage = "Username already exists";
+        }
+        model.addAttribute("errorMessage", errorMessage);
         return "reg";
+    }
+
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public String exceptionHandler(Exception e) {
+        LOG.error(e.getLocalizedMessage(), e);
+        return "redirect:/reg?error=true";
     }
 }
