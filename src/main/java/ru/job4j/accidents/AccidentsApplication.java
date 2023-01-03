fce4aeae35c316c4ada040d5eb79aba2f55a37ac
yarmail <yarmail@yandex.ru>
2023-01-03 17:22:29 +0300

    0. Spring test [#6881]

--- src/main/java/ru/job4j/accidents/AccidentsApplication.java
+++ src/main/java/ru/job4j/accidents/AccidentsApplication.java
@@ -2,7 +2,6 @@ package ru.job4j.accidents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AccidentsApplication {
