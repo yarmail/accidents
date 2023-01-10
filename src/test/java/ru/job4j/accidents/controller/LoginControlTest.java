a123e96dde4b908c0de1aebcbe5dd2e99b33bb92
yarmail <yarmail@yandex.ru>
2023-01-10 19:24:35 +0300

    0. Spring test [#6881 #377151]

--- src/test/java/ru/job4j/accidents/controller/LoginControlTest.java
+++ src/test/java/ru/job4j/accidents/controller/LoginControlTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.accidents.controller;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,25 +8,25 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class AccidentControllerTest {
+class LoginControlTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @WithMockUser
-    public void accidents() throws Exception {
-        this.mockMvc.perform(get("/"))
+    public void regPage() throws Exception {
+        this.mockMvc.perform(get("/login"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(view().name("accidents"));
+                .andExpect(view().name("login"));
     }
 }
